42a6eb4ef9e85c10790a3cb733909c0020ab6032
bendemers <ben.n.demers@gmail.com>
2018-09-26 08:48:01 -0400

    fix url

--- bot.py
+++ bot.py
@@ -9,10 +9,7 @@ import urllib
 found = False
 client = discord.Client()
 
-url='https://docs.google.com/spreadsheets/d/10cgQNGKzKVcu2dKs4zQgdthwCrwHimpI_Rst6mnulbI/export?format=csv'
-response = requests.get(url)
-with open(os.path.join("folder", "cookcove.csv"), 'wb') as f:
-    f.write(response.content)
+
 
 message_list = []
 message_count = 0
@@ -45,7 +42,11 @@ async def on_message(message):
                 member = message1.author
         server = client.get_server('475519440448913418')
         role = discord.utils.get(server.roles, name= 'monitor access')
-        with open('cookcove.csv', 'rt') as f:
+        url='https://docs.google.com/spreadsheets/d/10cgQNGKzKVcu2dKs4zQgdthwCrwHimpI_Rst6mnulbI/export?format=csv'
+        response = requests.get(url)
+        with open(os.path.join("folder", "cookcove.csv"), 'wb') as f:
+            f.write(response.content)
+        with open('folder/cookcove.csv', 'rt') as f:
             reader = csv.reader(f, delimiter=',')
             for row in reader:
                 if email == row[0]:  # if the username shall be on column 3 (-> index 2)
